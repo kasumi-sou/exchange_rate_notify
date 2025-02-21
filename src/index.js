4f5228707f0f989195d00502836559c2a91d08c4
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-22 00:28:47 +0900

    feat: send message on thread and logging error when error occurred

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const cron = require("node-cron");
 const { apiKey } = require("../config.json");
 const { webHookUrl } = require("../config.json");
+const { threadId } = require("../config.json");
 const { default: axios } = require("axios");
+const { setTimeout: sleep } = require("node:timers/promises");
 
 /*
 cron.schedule("0 39 18 * * *", async () => {
@@ -9,9 +11,64 @@ cron.schedule("0 39 18 * * *", async () => {
 });
 */
 
-cron.schedule("0 0 0,12,16,20 * * 1-5", async () => {
-	reportCurrency();
-	console.log("success!");
+// 月曜の処理
+cron.schedule("0 0 12-22/2 * * 1", async () => {
+	let result = null;
+	for (i = 0; i < 3; i++) {
+		try {
+			result = reportCurrency();
+			console.log(`success! (attempt #${i++})`);
+			break;
+		}
+		catch (e) {
+			console.error(e);
+		}
+		await sleep((2 ** i) * 1000);
+	}
+	if (!result) {
+		errorMessage();
+		return;
+	};
+});
+
+// 火～金
+cron.schedule("0 0 0,12-22/2 * * 2-5", async () => {
+	let result = null;
+	for (i = 0; i < 3; i++) {
+		try {
+			result = reportCurrency();
+			console.log(`success! (attempt #${i++})`);
+			break;
+		}
+		catch (e) {
+			console.error(e);
+		}
+		await sleep((2 ** i) * 1000);
+	}
+	if (!result) {
+		errorMessage();
+		return;
+	};
+});
+
+// 土曜
+cron.schedule("0 0 0,1 * * 6", async () => {
+	let result = null;
+	for (i = 0; i < 3; i++) {
+		try {
+			result = reportCurrency();
+			console.log(`success! (attempt #${i++})`);
+			break;
+		}
+		catch (e) {
+			console.error(e);
+		}
+		await sleep((2 ** i) * 1000);
+	}
+	if (!result) {
+		errorMessage();
+		return;
+	};
 });
 
 
@@ -25,6 +82,22 @@ async function reportCurrency() {
 	const utcTime = exchangeData.data["Realtime Currency Exchange Rate"]["6. Last Refreshed"];
 	const jstTime = new Date(utcTime + "Z").toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
 	const rate = parseFloat(exchangeData.data["Realtime Currency Exchange Rate"]["9. Ask Price"]).toFixed(3);
+
+	await axios({
+		url: webHookUrl + `?thread_id=${threadId}`,
+		method: "post",
+		headers: {
+			"Accept": "application/json",
+			"Content-type": "application/json",
+		},
+		data: {
+			content: `ドル/円為替レート:\n# ${rate} 円\n\n\`${jstTime}\` 現在 (attempt #${i++})`,
+		},
+	});
+	console.log(`${jstTime}: success!`);
+}
+
+async function errorMessage() {
 	await axios({
 		url: webHookUrl,
 		method: "post",
@@ -33,7 +106,8 @@ async function reportCurrency() {
 			"Content-type": "application/json",
 		},
 		data: {
-			content: `ドル/円為替レート:\n# ${rate} 円\n\n\`${jstTime}\` 現在`,
+			content: "処理に失敗しました！",
 		},
 	});
+	console.error("failed");
 }
